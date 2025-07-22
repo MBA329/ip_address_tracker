80bfa3defeb1e73b9dd7b05a1fa738af1264153c
emmanuel mba <emmanuelmba329@gmail.com>
2025-07-22 08:42:04 +0100

    fixedOmo

---
+++ src/hooks/useTracker.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+import {VITE_API_KEY} from "../constants";
+import {useQuery} from "@tanstack/react-query";
+
+
+ const useTracker = async (ip_address) => {
+    const response = await axios.get(
+        `https://geo.ipify.org/api/v2/country?apiKey=${VITE_API_KEY}&ipAddress=${ip_address}`
+    );
+
+    return response.data;
+};
+
+export const getTracker = ()=>{
+
+
+}
